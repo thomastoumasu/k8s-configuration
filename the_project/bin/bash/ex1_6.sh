d481368515e5558ec4232134aa8b317472431c13
thomastoumasu <thomastoumasu@protonmail.com>
2025-11-26 11:00:01 +0100

    ex 1.6 and updated 1.5

--- the_project/bin/bash/ex1_6.sh
+++ the_project/bin/bash/ex1_6.sh
@@ -6,10 +6,11 @@ k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 # create deployment and nodeport service
 kubectl apply -f manifests/deployment.yaml
 kubectl apply -f manifests/nodeport_service.yaml
-
 # kubectl delete -f manifests/deployment.yaml
 
 # check that app is accessible on host port
-POD=$(kubectl get pods -o=name | grep the-project)
-kubectl wait --for=condition=Ready $POD
-curl localhost:8082
+kubectl rollout status deployment the-project
+# POD=$(kubectl get pods -o=name | grep the-project)
+# kubectl wait --for=condition=Ready $POD
+
+sleep 1 && curl localhost:8082
