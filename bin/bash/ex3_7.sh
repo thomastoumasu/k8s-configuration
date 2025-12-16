e10ee4dff99e4e7a39ed257ff9df3fba20d5eccb
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-16 17:40:11 +0100

    start ex 3.7

---
+++ bin/bash/ex3_7.sh
@@ -0,0 +1,8 @@
+# 3.7 main deployment on namespace project and branch deployment on namespace branch
+
+# # create a static IP
+# gcloud compute addresses create my-ip --region=europe-north1 --network-tier=STANDARD
+# gcloud compute addresses describe my-ip --region=europe-north1
+# 35.217.26.181
+# use "www.thomastoumasu.dpdns.org" for main aka namespace project
+# use "www.thomastoumasu.xx.kg" for branch aka namespace branch
