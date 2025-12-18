b46e5cc18d5cc86afc05b3bd1bc80d0712eede0e
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-18 16:51:31 +0100

    s

--- bin/bash/ex3_8.sh
+++ bin/bash/ex3_8.sh
@@ -11,7 +11,7 @@
 
 # both deployment work and are accessible under different domains
 # can still be improved:
-# 1. Sometimes bind error on the volume shared between image-finder and frontend. Fix: Either use a ReadWriteMany, or put both apps in one pod.
+# 1. Sometimes bind error on the volume shared between image-finder and frontend. Fix: Either use a ReadWriteMany (but very expensive on GKE), or put both apps in one pod (loses the benefit of separatly scaling services).
 # 2. delete images programmatically
 # gcloud artifacts files list --project=dwk-gke-480809 --location=europe-north1 --repository=my-repository --package=backend --tag=1.0-dev
 # gcloud artifacts docker images list europe-north1-docker.pkg.dev/dwk-gke-480809/my-repository/backend --include-tags
@@ -21,3 +21,14 @@
 # gcloud compute addresses describe my-ip --region=europe-north1
 # gcloud compute addresses create my-ip --global
 # gcloud compute addresses describe my-ip --global  // 34.54.85.237
+
+gcloud artifacts repositories add-iam-policy-binding "my-repository" --project "dwk-gke-480809" --location "europe-north1" --member "serviceAccount:gcr-cleaner@my-project.iam.gserviceaccount.com" --role "roles/artifactregistry.repoAdmin"
+
+# clean up
+gcloud artifacts repositories set-cleanup-policies my-repository --project='dwk-gke-480809' --location='europe-north1' --policy='delete-policy.json' --no-dry-run
+
+# if dry run, see the result
+gcloud logging read 'protoPayload.serviceName="artifactregistry.googleapis.com" AND protoPayload.request.parent="projects/dwk-gke-480809/locations/europe-north1/repositories/my-repository/packages/-" AND protoPayload.request.validateOnly=true' --resource-names="projects/dwk-gke-480809" --project=dwk-gke-480809
+
+# check after one day
+gcloud artifacts docker images list europe-north1-docker.pkg.dev/dwk-gke-480809/my-repository/backend --include-tags > after.txt
