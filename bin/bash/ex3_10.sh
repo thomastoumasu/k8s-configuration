5067a46eb67eef9b172a9cfe6c0638c1a265f813
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-20 13:51:46 +0100

    deploy

--- bin/bash/ex3_10.sh
+++ bin/bash/ex3_10.sh
@@ -1,5 +1,5 @@
 # 3.10 save to Google GLobal Storage
+# set up backup_dl.yaml as cron job github action (needs cluster up and working, for example with sh create_gkecl_big_gat.sh)
 
-gcloud storage cp test-storage2.txt gs://thomastoumasu_k8s-bucket
-
-docker run --rm -it --name frontend -p 5173:80 frontend bash
+# template for storing local file testfile.txt to a public bucket:
+# gcloud storage cp testfile.txt gs://thomastoumasu_k8s-bucket
