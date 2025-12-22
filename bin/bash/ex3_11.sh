cb241ae03ab0fd322669d512286bc1760050db76
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-22 14:59:53 +0100

    Ex 3.11

--- bin/bash/ex3_11.sh
+++ bin/bash/ex3_11.sh
@@ -12,6 +12,21 @@
 #     limits: # kubelet enforces the limit so that the running container is not allowed to use more of that resource:
 #       memory: "128Mi" # Above this, the kernel may terminate the container (OOMKilled, Out Of Memory) when it detects memory pressure. 
 #       cpu: "500m" # Around this, the kernel will restrict access to the CPU (CPU throttling).
+# for mongo request, check https://www.mongodb.com/docs/kubernetes/current/tutorial/plan-k8s-op-considerations/
+
+# result:
+# (base) thomas@thomass-Air k8s-submission % kubectl top pod -l app=backend
+# kubectl top pod -l app=frontend
+# kubectl top pod -l app=mongo
+# kubectl top pod -l app=image-finder
+# NAME                           CPU(cores)   MEMORY(bytes)   
+# backend-dep-7c69995bc5-4bx8v   3m           50Mi            
+# NAME                            CPU(cores)   MEMORY(bytes)   
+# frontend-dep-546bbfc758-zlt9v   1m           3Mi             
+# NAME         CPU(cores)   MEMORY(bytes)   
+# mongo-ss-0   11m          176Mi           
+# NAME                                CPU(cores)   MEMORY(bytes)   
+# image-finder-dep-6494c756db-k6bt7   5m           38Mi  
 
 CLUSTER_NAME=dwk-cluster
 LOCATION=europe-north1-b
@@ -35,9 +50,7 @@ kubectl create namespace project
 gcloud container clusters get-credentials $CLUSTER_NAME --location=$CONTROL_PLANE_LOCATION
 
 # check: Each node has a maximum capacity for each of the resource types: the amount of CPU and memory it can provide for Pods. 
-kubectl get nodes
-kubectl describe nodes 
-kubectl describe node/
+kubectl describe nodes > nodesStart.txt
 
 # push on main to deploy project on namespace project (see .github/workflows/deploy_the-project.yaml)
 
@@ -51,71 +64,13 @@ kubectl logs $BACKENDPOD
 
 # get pods status
 POD=$BACKENDPOD
-kubectl get pods $POD --no-headers -o custom-columns=":status.phase"
-kubectl get pod $POD --output=yaml
-kubectl describe pod $POD
-# how much does it use
-kubectl top pod $POD --namespace=cpu-example
-kubectl describe nodes
+kubectl get $POD --no-headers -o custom-columns=":status"
+kubectl get $POD --output=yaml
+kubectl describe $POD
 
-# # debug
-# kubectl delete -f ./the_project/mongo/manifests/dumper.yaml
-kubectl describe cronjobs/periodic-dumper
-kubectl describe job/periodic-dumper-29440029
-kubectl logs pod/periodic-dumper-29440029-ghq95 # should see the dump: Copying file:///usr/src/app/dump/the_database/todos-2025-12-22T11-09-01.bson to gs://thomastoumasu_k8s-bucket/todos-2025-12-22T11-09-01.bson
-# kubectl describe pod/dumper 
-# kubectl logs -f pod/dumper 
-# kubectl exec -it dumper -- sh 
-#   BUCKET="thomastoumasu_k8s-bucket"
-#   URI="mongodb://the_username:the_password@mongo-svc.project:27017/the_database"
-#   mongodump --uri=$MONGO_URI --out /usr/src/app/dump/
-#   NOW=$(date +'%Y-%m-%dT%H-%M-%S')
-#   FILENAME="/usr/src/app/dump/the_database/todos-${NOW}.bson"
-#   mv /usr/src/app/dump/the_database/todos.bson $FILENAME
-#   curl -X GET -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://storage.googleapis.com/storage/v1/b/${BUCKET}/o"
-#   gcloud storage cp $FILENAME gs://${BUCKET}
-
-
-# # debug: additional permissions
-# gcloud storage buckets add-iam-policy-binding gs://${BUCKET} \
-#     --role=roles/storage.objectViewer \
-#     --member=principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${PROJECT_ID}.svc.id.goog/subject/ns/project/sa/${KSA_NAME} \
-#     --condition=None
-
-# gcloud storage buckets add-iam-policy-binding gs://${BUCKET} \
-#     --role=roles/storage.objectCreator \
-#     --member=principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${PROJECT_ID}.svc.id.goog/subject/ns/project/sa/${KSA_NAME} \
-#     --condition=None
-
-# gcloud storage buckets add-iam-policy-binding gs://${BUCKET} \
-#     --role=roles/storage.admin \
-#     --member=principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${PROJECT_ID}.svc.id.goog/subject/ns/project/sa/${KSA_NAME} \
-#     --condition=None
-
-# gcloud storage buckets add-iam-policy-binding gs://${BUCKET} \
-#     --role=roles/storage.objectAdmin \
-#     --member=principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${PROJECT_ID}.svc.id.goog/subject/ns/project/sa/${KSA_NAME} \
-#     --condition=None
-
-
-# # debug: other way
-###
-# gcloud iam service-accounts create access-gcs \
-#     --project=${PROJECT_ID}
-
-# gcloud projects add-iam-policy-binding $PROJECT_ID \
-#     --member "serviceAccount:access-gcs@${PROJECT_ID}.iam.gserviceaccount.com" \
-#     --role "roles/storage.admin"
-
-# gcloud container clusters get-credentials $CLUSTER_NAME --zone $LOCATION --project $PROJECT_ID
-
-# kubectl create serviceaccount gcs-access-ksa \
-#     --namespace project
-
-# gcloud iam service-accounts add-iam-policy-binding access-gcs@${PROJECT_ID}.iam.gserviceaccount.com \
-#     --role roles/iam.workloadIdentityUser \
-#     --member "serviceAccount:${PROJECT_ID}.svc.id.goog[project/gcs-access-ksa]"
-
-# kubectl annotate serviceaccount gcs-access-ksa \
-#     --namespace project \
-#     iam.gke.io/gcp-service-account=access-gcs@${PROJECT_ID}.iam.gserviceaccount.com
+# how much do the apps use
+kubectl top pod -l app=backend
+kubectl top pod -l app=frontend
+kubectl top pod -l app=mongo
+kubectl top pod -l app=image-finder
+kubectl describe nodes > nodesEnd.txt
