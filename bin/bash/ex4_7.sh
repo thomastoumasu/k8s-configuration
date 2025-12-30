d1333ddf95db0d2f8d3f2f903d0e7c2a044f67d9
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-30 12:09:05 +0100

    added kustomize for log_output

---
+++ bin/bash/ex4_7.sh
@@ -0,0 +1,46 @@
+# 4.7 deploy pingpong app with argo
+# https://courses.mooc.fi/org/uh-cs/courses/devops-with-kubernetes/chapter-5/gitops
+
+# run like this
+CLUSTER_NAME=dwk-cluster
+LOCATION=europe-north1-b
+CONTROL_PLANE_LOCATION=europe-north1-b
+PROJECT_ID=dwk-gke-480809
+PROJECT_NUMBER=267537331918
+# create cluster
+gcloud -v
+gcloud auth login
+gcloud config set project $PROJECT_ID
+gcloud services enable container.googleapis.com
+gcloud container clusters create $CLUSTER_NAME --zone=$LOCATION \
+  --cluster-version=1.32 --disk-size=32 --num-nodes=3 --machine-type=e2-small \
+  --gateway-api=standard
+kubectl cluster-info
+kubectl create namespace exercises
+# set kube-config to point at the cluster
+gcloud container clusters get-credentials $CLUSTER_NAME --location=$CONTROL_PLANE_LOCATION
+
+# # kustomize sanity check
+# kubectl kustomize .
+# deploy using kustomize
+kubens exercises
+kubectl apply -k .
+# use argo
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# check external IP of argocd-server
+kubectl get svc -n argocd --watch
+# get initial password for admin
+kubectl get -n argocd secrets argocd-initial-admin-secret -o yaml
+# base64 decode it
+kubectl get -n argocd secrets argocd-initial-admin-secret -o yaml | grep -o 'password: .*' | cut -f2- -d: | base64 --decode
+# log in using external IP using admin and this password
+
+# get gateway IP
+kubectl get gateway pingpong-gateway --watch
+# debug
+kubectl describe gateway pingpong-gateway
+kubectl get httproutes
+kubectl describe httproutes log-output-route
+
